c43e2d9597fa246891ebf64b5ec9e9fd2aa2d666
JoseCarlos-Filho <etijcvivi@gmail.com>
2024-12-16 17:56:29 -0300

    feat: removendo comentário de teste

--- js/index.js
+++ js/index.js
@@ -1,8 +1,2 @@
 import carrosel from "./carrosel.js";
-//require('dotenv').config()
-// import { dotenv } from "../node_modules/dotenv";
-// dotenv.config();
-// const apikey = process.env.API_KEY;
-// console.log(apikey);
-// const apikey = window.process?.env?.API_KEY
 carrosel();
