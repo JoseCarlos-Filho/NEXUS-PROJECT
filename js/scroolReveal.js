e03daa7427935fe900277aa2d3f503b9305ea24e
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-01-08 19:33:00 -0300

    feat: adicionando scrollreveal nos elementos do carrosel e depoimento.

--- js/scroolReveal.js
+++ js/scroolReveal.js
@@ -1,20 +1,22 @@
 export default function scroolR() {
     window.revelar = ScrollReveal({reset: true});
     // topo do site
+
     revelar.reveal('.scroll-efect', 
     {
-        duration: 3000, 
+        duration: 2000, 
         origin: 'top', 
         distance: '100px',
         delay: 500
     });
 
     // banner page
+
     revelar.reveal('.scroll-banner-efect', 
     {
         duration: 2000, 
         origin: 'left', 
-        distance: '100px',
+        distance: '500px',
         delay: 500
     });
 
@@ -22,17 +24,50 @@ export default function scroolR() {
     {
         duration: 2000, 
         origin: 'right', 
-        distance: '100px',
+        distance: '500px',
         delay: 1500
     });
 
     // carrossel
+
+    revelar.reveal('.scroll-carrossel-titulo-efect', 
+    {
+        duration: 2000, 
+        distance: '100px',
+        delay: 500
+    });
+
     revelar.reveal('.scroll-carrossel-efect', 
     {
         duration: 2000, 
         origin: 'left', 
         distance: '100px',
+        delay: 1000
+    });
+
+    // titulo e conteudo de depoimento 
+    
+    revelar.reveal('.scroll-content-titulo-efect', 
+    {
+        duration: 2000, 
+        origin: 'left', 
+        distance: '400px',
         delay: 500
     });
     
+    revelar.reveal('.scroll-content-descricao-efect', 
+    {
+        duration: 2000, 
+        origin: 'right', 
+        distance: '400px',
+        delay: 1000
+    });
+
+    revelar.reveal('.scroll-depoimentos-efect', 
+    {
+        duration: 2000, 
+        // origin: 'right', 
+        distance: '400px',
+        delay: 1500
+    });
 }
