12807e7352f66cf871cb7aa3b2c94cc09f84be0e
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-01-08 17:08:49 -0300

    feat: adicionando novas classes de animação na classe scroolR

--- js/scroolReveal.js
+++ js/scroolReveal.js
@@ -3,24 +3,36 @@ export default function scroolR() {
     // topo do site
     revelar.reveal('.scroll-efect', 
     {
-        duration: 2000, 
+        duration: 3000, 
         origin: 'top', 
-        distance: '100px'
+        distance: '100px',
+        delay: 500
     });
 
     // banner page
     revelar.reveal('.scroll-banner-efect', 
     {
-        duration: 3000, 
+        duration: 2000, 
         origin: 'left', 
-        distance: '100px'
+        distance: '100px',
+        delay: 500
     });
 
+    revelar.reveal('.scroll-banner-img-efect', 
+    {
+        duration: 2000, 
+        origin: 'right', 
+        distance: '100px',
+        delay: 1500
+    });
+
+    // carrossel
     revelar.reveal('.scroll-carrossel-efect', 
     {
-        duration: 3000, 
+        duration: 2000, 
         origin: 'left', 
-        distance: '100px'
+        distance: '100px',
+        delay: 500
     });
     
 }
