ab04bc34129815d9d0db1757b49f4b1bc2ca7534
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-01-08 21:27:26 -0300

    feat: adicionando checagem de tamanho de tela para execução do scrollreleaser

--- js/scroolReveal.js
+++ js/scroolReveal.js
@@ -1,73 +1,75 @@
 export default function scroolR() {
-    window.revelar = ScrollReveal({reset: true});
-    // topo do site
+    if(window.innerWidth > 1200) {
+        window.revelar = ScrollReveal({reset: true});
+        // topo do site
 
-    revelar.reveal('.scroll-efect', 
-    {
-        duration: 2000, 
-        origin: 'top', 
-        distance: '100px',
-        delay: 500
-    });
+        revelar.reveal('.scroll-efect', 
+        {
+            duration: 2000, 
+            origin: 'top', 
+            distance: '100px',
+            delay: 500
+        });
 
-    // banner page
+        // banner page
 
-    revelar.reveal('.scroll-banner-efect', 
-    {
-        duration: 2000, 
-        origin: 'left', 
-        distance: '100px',
-        delay: 500
-    });
+        revelar.reveal('.scroll-banner-efect', 
+        {
+            duration: 2000, 
+            origin: 'left', 
+            distance: '300px',
+            delay: 500
+        });
 
-    revelar.reveal('.scroll-banner-img-efect', 
-    {
-        duration: 2000, 
-        origin: 'right', 
-        distance: '100px',
-        delay: 1500
-    });
+        revelar.reveal('.scroll-banner-img-efect', 
+        {
+            duration: 2000, 
+            origin: 'right', 
+            distance: '300px',
+            delay: 1500
+        });
 
-    // carrossel
+        // carrossel
 
-    revelar.reveal('.scroll-carrossel-titulo-efect', 
-    {
-        duration: 2000, 
-        distance: '100px',
-        delay: 500
-    });
+        revelar.reveal('.scroll-carrossel-titulo-efect', 
+        {
+            duration: 2000, 
+            distance: '300px',
+            delay: 500
+        });
 
-    revelar.reveal('.scroll-carrossel-efect', 
-    {
-        duration: 2000, 
-        origin: 'left', 
-        distance: '100px',
-        delay: 1000
-    });
+        revelar.reveal('.scroll-carrossel-efect', 
+        {
+            duration: 2000, 
+            origin: 'left', 
+            distance: '300px',
+            delay: 1000
+        });
 
-    // titulo e conteudo de depoimento 
-    
-    revelar.reveal('.scroll-content-titulo-efect', 
-    {
-        duration: 2000, 
-        origin: 'left', 
-        distance: '100px',
-        delay: 500
-    });
-    
-    revelar.reveal('.scroll-content-descricao-efect', 
-    {
-        duration: 2000, 
-        origin: 'right', 
-        distance: '100px',
-        delay: 1000
-    });
+        // titulo e conteudo de depoimento 
+        
+        revelar.reveal('.scroll-content-titulo-efect', 
+        {
+            duration: 2000, 
+            origin: 'left', 
+            distance: '300px',
+            delay: 200
+        });
+        
+        revelar.reveal('.scroll-content-descricao-efect', 
+        {
+            duration: 2000, 
+            origin: 'right', 
+            distance: '300px',
+            delay: 500
+        });
 
-    revelar.reveal('.scroll-depoimentos-efect', 
-    {
-        duration: 2000, 
-        // origin: 'right', 
-        distance: '100px',
-        delay: 1500
-    });
+        revelar.reveal('.scroll-depoimentos-efect', 
+        {
+            duration: 2000, 
+            // origin: 'right', 
+            distance: '300px',
+            delay: 800
+        });
+    };
 }
